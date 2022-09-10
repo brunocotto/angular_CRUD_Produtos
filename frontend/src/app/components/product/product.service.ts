8b51551c5590d3427c453af95934c063bdbbd7f3
brunocotto <bruno.ottoo@gmail.com>
2022-09-10 20:12:39 -0300

    tratamento de erro adicionado product.service

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,9 +1,12 @@
+import { map, catchError} from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 import { Product } from './product.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Observable } from 'rxjs';
 import { UrlSegmentGroup } from '@angular/router';
+import { EmptyExpr } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -15,39 +18,60 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
-    this.snackBar.open(msg, 'X', {
+  showMessage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, ' ', {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-sucess']
     })
   }
   //Mandando uma requisição HTTP para o backend
   //Utilizando o modelo de produto criado no product.model.ts
   //Notação generics => Observable<Product> => Retorna um observable do tipo produto
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseURL, product)
+    return this.http.post<Product>(this.baseURL, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   //lendo dados do back
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseURL);
+    return this.http.get<Product[]>(this.baseURL).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   //search for ID
   readById(id: string): Observable<Product> {
     //interpolação da rota de produtos com o ID, retornando um Observable
     const url = `${this.baseURL}/${id}`;
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
   //update for ID
   update(product: Product): Observable<Product> {
     const url = `${this.baseURL}/${product.id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
   //delete for ID
   delete(id: number): Observable<Product> {
     const url = `${this.baseURL}/${id}`;
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+  //tratamento de erro
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro', true);
+    return EMPTY;
   }
 }
